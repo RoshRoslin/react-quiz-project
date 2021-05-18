7686ade18bb7f9f09165df2493d5260d2dde59ba
RoshRoslin <RoshRoslin@gmail.com>
2021-05-18 16:59:33 -0700

    Updates

---
+++ precache-manifest.ff628bcfbd2e84cc7862abbf3591e695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062585a47a16a9ab753d0619d95a9697",
+    "url": "/react-quiz-project/index.html"
+  },
+  {
+    "revision": "ea6d8ec63b32149b576f",
+    "url": "/react-quiz-project/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/react-quiz-project/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-quiz-project/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6d8ec63b32149b576f",
+    "url": "/react-quiz-project/static/js/main.03b6d40e.chunk.js"
+  },
+  {
+    "revision": "1e11a682c11a29dc939b",
+    "url": "/react-quiz-project/static/js/runtime-main.2e56501b.js"
+  }
+]);
